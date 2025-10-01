8fd896f2d23947ae294478c8e18f1f05b133482d
AliAbdur1 <AliAbdur171@gmail.com>
2025-09-30 23:51:27 -0400

    config file adjust 2

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  root: 'src/',
   publicDir: 'assets/static',
-  base: './',
   plugins: [react()],
 })
